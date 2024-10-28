2a4d670bd12eec80007cecec1eca00fc23e817ef
Yuri Ribeiro <yurimaicon2@outlook.com>
2024-10-28 19:59:44 -0300

    fix structure

--- book-service/src/test/java/org/example/bookservice/BookServiceApplicationTests.java
+++ book-service/src/test/java/org/example/bookservice/BookServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.bookshop;
+package org.example.bookservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookShopApplicationTests {
+class BookServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
